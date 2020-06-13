2e6ae366567e14a4f6e65615cf007f25be809caa
Paul K Lynch <dramatichamster@gmail.com>
2020-06-13 12:15:44 +1000

    Small cleanup of pre-set styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Depression Boy`,
+    description: `Depression Boy website`,
+    author: `@A-A-Z`,
   },
   plugins: [
     `gatsby-plugin-sass`,
